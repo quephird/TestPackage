0ee63d7cfd11b6f72554c49187ee8c239839fb59
quephird <danielle_kefford@hotmail.com>
2022-09-22 15:20:49 -0700

    Added new test component.

---
+++ Sources/TestPackage/TestComponent.swift
@@ -0,0 +1,12 @@
+//
+//  TestComponent.swift
+//  
+//
+//  Created by Danielle Kefford on 9/22/22.
+//
+
+public struct TestComponent {
+    public static func sayHello(_ name: String) -> String {
+        return "Hello, \(name)"
+    }
+}
